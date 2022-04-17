15b851afd94d18b4d12d125b558d3aa325c2a421
BerciBAA <bercike222@gmail.com>
2022-04-17 17:32:25 +0200

    Házi feladat: a stream-ek használata (2)

--- src/main/java/brickset/LegoSetRepository.java
+++ src/main/java/brickset/LegoSetRepository.java
@@ -2,7 +2,10 @@ package brickset;
 
 import repository.Repository;
 
-import java.util.Comparator;
+import java.util.Map;
+
+import static java.util.stream.Collectors.*;
+
 
 /**
  * Represents a repository of {@code LegoSet} objects.
@@ -12,71 +15,89 @@ public class LegoSetRepository extends Repository<LegoSet> {
     public LegoSetRepository() {
         super(LegoSet.class, "brickset.json");
     }
-    public long countLegoSetsWithTag(String tag) {
+    public void printAll() {
+         getAll().stream().forEach(System.out::println);
+
+    }
+
+    /**
+     *
+     * @param name String tipus. Nevet kell megadni a készletekből.
+     * @return Leerenőrzi hogy a name paraméter szerepel-e a lego készletekben.
+     */
+    public boolean anyMatchName(String name){
+        return getAll()
+                .stream()
+                .anyMatch(l -> l.getName().contains(name));
+    }
+
+    /**
+     * Kiírja az összes tag-et egyedi módon tehát nem szerepel benne kétszer ugyan az.
+     */
+    public void printAllTags() {
+        getAll().stream()
+                .filter(l -> l.getTags()!=null)
+                .flatMap(l -> l.getTags().stream())
+                .distinct()
+                .forEach(System.out::println);
+
+    }
+
+    /**
+     *
+     * @return Vissza adja a lego készleteknek a darbjainak az összegét
+     */
+    public int legoSetPiecesSum(){
         return getAll().stream()
-                .filter(legoSet -> legoSet.getTags() != null && legoSet.getTags().contains(tag))
-                .count();
+                       .map(l -> l.getPieces())
+                       .reduce(0, (a, b) -> a + b);
     }
 
     /**
-     * Megszámolja hogy egy lego készletből hány ilyen tipusú
-     * @param themename megtudsz adni egy lego témát
-     * @return vissza adja hogy egy adott témában hány olyan tipusú lego szett van
+     *
+     * @param greater Int tipusú. nagyobbnak kell lenni az adott értéknél
+     * @return Megadja hogy a greater változónál hány kisebb és hány nagyyobb elem van
      */
-    public long countLegotheme(String themename) {
-        return getAll().stream().filter(theme -> theme.getTheme() != null && theme.getTheme().contains(themename)).count();
-
+    public Map<Boolean, Long> greaterThan(int greater) {
+        return getAll().stream()
+                .collect(partitioningBy(l -> l.getPieces() > greater, counting()));
     }
 
     /**
-     * Kilistázza egy adott témához tartozó lego készleteknek az adatait
-     * @param themename Adott témájú lego
+     *
+     * @param smaller Int tipusú. kissebbnek kell lenni az adott értéknél
+     * @return Megadja hogy a smaller változónál hány kisebb és hány nagyyobb elem van
      */
-    public  void printLegotheme(String themename) {
-        getAll().stream().filter(theme -> theme.getTheme() != null && theme.getTheme().contains(themename)).forEach(System.out::println);
-
+    public Map<Boolean, Long> smallerThan(int smaller) {
+        return getAll().stream()
+                .collect(partitioningBy(l -> l.getPieces() < smaller, counting()));
     }
 
     /**
-     * Vissza adja azokat a lego készleteket amikben több darab van mint a megadott szám
-     * @param moreThanPieces Egy int tipusú szám
-     * @return Vissza adja azokat a lego készleteket amikben több darab van mint a megadott szám
+     *
+     * @return Visszaadja hogy hány nulla elemű lego készlet van
      */
-    public long countMoreLegoPieciesthan(int moreThanPieces)
-    {
-        return getAll().stream().filter(pieces -> pieces.getPieces() > moreThanPieces ).count();
-    }
-
-    /**
-     * Kíírja a paraméternél nagyobb számú darabokból álló lego készletek nevét
-     * @param moreThanPieces Egy int tipusú szám
-     */
-    public void printMoreLegoPieciesthan(int moreThanPieces)
-    {
-        getAll().stream().filter(pieces -> pieces.getPieces() > moreThanPieces).sorted().map(legoSet -> legoSet.getPieces()+" "+legoSet.getName() ).forEach(System.out::println);
-    }
-
-    /**
-     * Az összes lészletnek a darabjainak az átlagát adja vissza
-     * @return Az összes lészletnek a darabjainak az átlagát adja vissza
-     */
-    public double averagePieceNumber(){
-        return getAll().stream().mapToLong(LegoSet::getPieces).average().getAsDouble();
-    }
-
-    /**
-     * Névszerint kilistázza a lego készleteket
-     */
-    public void printNamesSorted(){
-        getAll().stream().map(LegoSet::getName).sorted().forEach(System.out::println);
+    public Map<Boolean, Long> piecesZero() {
+        return getAll().stream()
+                .collect(partitioningBy(l -> l.getPieces() == 0 , counting()));
     }
     public static void main(String[] args) {
         var repository = new LegoSetRepository();
-        System.out.println(repository.countLegoSetsWithTag("Microscale"));
-        System.out.println(repository.countLegotheme("Duplo"));
-        repository.printLegotheme("Duplo");
-        System.out.println(repository.countMoreLegoPieciesthan(100));
-        System.out.println(repository.averagePieceNumber());
-        repository.printNamesSorted();
+        repository.printAll();
+        System.out.println("----");
+        System.out.println(repository.anyMatchName("Lighthouse"));
+        System.out.println("----");
+        System.out.println(repository.legoSetPiecesSum());
+        System.out.println("----");
+        repository.printAllTags();
+        System.out.println("----");
+        System.out.println(repository.greaterThan(100));
+        System.out.println("----");
+        System.out.println(repository.smallerThan(100));
+        System.out.println("----");
+        System.out.println(repository.piecesZero());
+        System.out.println("----");
+
+
     }
 }
